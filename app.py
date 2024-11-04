063cb68511af8d6fb0fcb95e4b55c16bb8dd1890
Silvio Lima <silviolima07@gmail.com>
2024-11-04 11:25:56 -0300

    Update

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ from prophet import Prophet
 
 import pandas as pd
 from xgboost import XGBRegressor
-from scikit-learn.model_selection import TimeSeriesSplit
-from scikit-learn.metrics import mean_squared_error
+from scikit_learn.model_selection import TimeSeriesSplit
+from scikit_learn.metrics import mean_squared_error
 import numpy as np
 #np.float_ = np.float64
 
